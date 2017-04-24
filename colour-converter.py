212b38a83ba5265bb5027a5f4924cd92393811e9
Sam <knutsen@jetspace.co>
2017-04-24 14:07:33 +0100

    ff is 1515 not 15 + 15 (32)
    Returns string not integer so numerical addition does not take place, rather adds the strings together, self.coef is max of R, G, or B divided by total of hex (1515) not 32

--- colour-converter.py
+++ colour-converter.py
@@ -1,29 +1,29 @@
 def hexdic(base):
     base = str(base)
     return {
-        "0": 0,
-        "1": 1,
-        "2": 2,
-        "3": 3,
-        "4": 4,
-        "5": 5,
-        "6": 6,
-        "7": 7,
-        "8": 8,
-        "9": 9,
-        "a": 10,
-        "b": 11,
-        "c": 12,
-        "d": 13,
-        "e": 14,
-        "f": 15,
+        "0": "0",
+        "1": "1",
+        "2": "2",
+        "3": "3",
+        "4": "4",
+        "5": "5",
+        "6": "6",
+        "7": "7",
+        "8": "8",
+        "9": "9",
+        "a": "10",
+        "b": "11",
+        "c": "12",
+        "d": "13",
+        "e": "14",
+        "f": "15",
     }.get(base, base)
 
 
 class ColourConvert(object):
     def __init__(self, *colour):
         self.colour = colour
-        self.coef = 255.0/30.0
+        self.coef = 255.0/1515.0
 
     def toRGB(self):
         hex = self.colour[0]
